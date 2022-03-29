35f94419c91c808103c6242eadd0cfe5b5a63ccc
diamba76 <102378802+diamba76@users.noreply.github.com>
2022-03-29 14:44:32 -0300

    Departamentos, Idade

--- funcionarios/models.py
+++ funcionarios/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100, help_text='Nome do Funcionário')
+    idade = models.PositiveIntegerField(default=0, blank=True, null=False, help_text='Idade do Funcionário')
 
     def __str__(self):
         return self.nome
