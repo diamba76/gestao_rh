d90e64db4d77068ef8f8360bf4f7cb8735d7f9b4
diamba76 <102378802+diamba76@users.noreply.github.com>
2022-04-01 14:13:42 -0300

    Funcionario

--- documentos/models.py
+++ documentos/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from funcionarios.models import Funcionario
+
 
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
+    pertencente = models.ForeignKey(Funcionario, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.descricao
--- funcionarios/models.py
+++ funcionarios/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from departamentos.models import Departamento
+from empresas.models import Empresa
 
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100, help_text='Nome do Funcionário')
     idade = models.PositiveIntegerField(default=0, blank=True, null=False, help_text='Idade do Funcionário')
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
+
 
     def __str__(self):
         return self.nome
